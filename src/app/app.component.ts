4d64e59511c9015d95614d0a90e5c6c44ba51f7b
JeromeNL <jw.kwetters@student.avans.nl>
2023-04-25 16:09:58 +0200

    15. Remove a task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,17 @@ export class AppComponent {
   add(newTask: string){
     this.tasks.push(newTask);
   }
+
+  remove(existingTask: string){
+    var userConfirmed = confirm(`Weet je zeker dat je het volgende item wilt verwijderen? \n "${existingTask}"`)
+    if(userConfirmed) {
+      // for (let i = 0; i < this.tasks.length; i++) {
+      //   if (this.tasks[i] === existingTask) {
+      //     delete this.tasks[i];
+      //   }
+      // }
+
+      this.tasks = this.tasks.filter(task => task != existingTask);
+    }
+  }
 }
