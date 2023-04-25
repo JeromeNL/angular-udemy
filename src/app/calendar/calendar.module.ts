6cfbad695657f320dea3378ca3d2df12b46d68a0
JeromeNL <jw.kwetters@student.avans.nl>
2023-04-25 19:23:12 +0200

    26. Style Angular calendar

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
-
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { MatDatepickerModule} from "@angular/material/datepicker";
+import { MatNativeDateModule} from "@angular/material/core";
 
 
 @NgModule({
@@ -9,7 +11,10 @@ import { CalendarComponent } from './calendar.component';
     CalendarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ], exports: [
     CalendarComponent
   ]
