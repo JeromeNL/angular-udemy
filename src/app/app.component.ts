b6ef4c836515ae613a809067bc31d570c9569e98
JeromeNL <jw.kwetters@student.avans.nl>
2023-04-25 16:12:37 +0200

    16. Mark as done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,13 +21,11 @@ export class AppComponent {
   remove(existingTask: string){
     var userConfirmed = confirm(`Weet je zeker dat je het volgende item wilt verwijderen? \n "${existingTask}"`)
     if(userConfirmed) {
-      // for (let i = 0; i < this.tasks.length; i++) {
-      //   if (this.tasks[i] === existingTask) {
-      //     delete this.tasks[i];
-      //   }
-      // }
-
       this.tasks = this.tasks.filter(task => task != existingTask);
     }
+  }
+
+  done(existingTask: string){
+
   }
 }
