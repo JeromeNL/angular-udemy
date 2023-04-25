1c1c53ab19b735462079d5e91670144f41c80a00
JeromeNL <jw.kwetters@student.avans.nl>
2023-04-25 15:51:36 +0200

    11. ngForLoop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks = [
+    "Visit Ann",
+    "Call Dad",
+    "Go to the gym",
+    "Homework"
+  ]
 }
